c001797475f653579fb2688d0c10373ef9e2e6ca
TylaAllyDare <tyladare@icloud.com>
2024-09-03 19:24:11 +0200

    3D model

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 
-const App=() => {
-
-  return (
-   <main className="bg-black">
-    <Navbar />
-    <Hero />
-    <Highlights />
+  const App = () => {
+    return (
+      <main className="bg-black">
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
    </main>
   )
 }
